e822a29028b10d7cc72b509b2ee9da135eebcc44
Michael Wu <michael.yixuan.wu@gmail.com>
2013-10-05 19:12:59 -0400

    Adding GUI for downloading files

--- Peerster/FilePanel.cc
+++ Peerster/FilePanel.cc
@@ -1,13 +1,28 @@
 #include "FilePanel.hh"
 
+const QString FilePanel::button1text = "Add Files(s)";
+const QString FilePanel::button2text = "Download File";
+
 FilePanel::FilePanel() {
     fileShareBox = new QGroupBox("File Sharing");
     fileShareBoxLayout = new QVBoxLayout;
-    QPushButton *selectFiles = new QPushButton("Add File(s)");
-    connect(selectFiles, SIGNAL(pressed()), this, SLOT(showDialog()));
-    fileShareBoxLayout->addWidget(selectFiles);
     fileShareBox->setLayout(fileShareBoxLayout);
     
+    // Create signal mapper
+    signalMapper = new QSignalMapper(this);
+    connect(signalMapper, SIGNAL(mapped(QString)), this, SLOT(buttonClicked(QString)));
+    
+    // Add files button
+    selectFilesButton = new QPushButton(button1text);
+    fileShareBoxLayout->addWidget(selectFilesButton);
+    signalMapper->setMapping(selectFilesButton, button1text);
+    connect(selectFilesButton, SIGNAL(clicked()), signalMapper, SLOT(Map()));
+    
+    // Download file button
+    downloadFileButton = new QPushButton(button2text);
+    fileShareBoxLayout->addWidget(downloadFileButton);
+    signalMapper->setMapping(downloadFileButton, button2text);
+    connect(downloadFileButton, SIGNAL(clicked()), signalMapper, SLOT(Map()));
 }
 
 QGroupBox* FilePanel::getGroupBox() {
@@ -15,7 +30,7 @@ QGroupBox* FilePanel::getGroupBox() {
 }
 
 void FilePanel::showDialog() {
-   QStringList filesToAdd = QFileDialog::getOpenFileNames();
+    QStringList filesToAdd = QFileDialog::getOpenFileNames();
     for (int i = 0; i < filesToAdd.size(); i++) {
         qDebug() << "Scanning file: " + filesToAdd.at(i);
         files.append(new PeersterFile(filesToAdd.at(i)));
--- Peerster/FilePanel.hh
+++ Peerster/FilePanel.hh
@@ -7,6 +7,7 @@
 #include <QGroupBox>
 #include <QList>
 #include <QPushButton>
+#include <QSignalMapper>
 #include <QVBoxLayout>
 
 #include "PeersterFile.hh"
@@ -22,10 +23,16 @@ public:
     void showDialog();
     
 private:
+    static const QString button1text;
+    static const QString button2text;
+    static const QString button3text;
+    QPushButton *downloadFileButton;
     QGroupBox *fileShareBox;
     QVBoxLayout *fileShareBoxLayout;
     QFileDialog *fileDialog;
     QList<PeersterFile*> files;
+    QSignalMapper *signalMapper;
+    QPushButton *selectFilesButton;
 };
 
 #endif
