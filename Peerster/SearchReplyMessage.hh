3da1b64505a4c471d98589fb441faf9569173a30
Michael Wu <michael.yixuan.wu@gmail.com>
2013-10-10 12:32:42 -0400

    added search reply message class

---
+++ Peerster/SearchReplyMessage.hh
@@ -0,0 +1,15 @@
+#ifndef __Peerster__SearchReplyMessage__
+#define __Peerster__SearchReplyMessage__
+
+#include <QVariantList>
+
+#include "Message.hh"
+
+class SearchReplyMessage : public Message {
+    SearchReplyMessage(const QString someOrigin, const QString someDest, const quint32 someHoplimit, const QString someSearchReply, const QVariantList someMatchNames, const QVariantList someMatchIDs);
+    
+protected:
+    QByteArray serializeSearchReplyMessage();
+};
+
+#endif
